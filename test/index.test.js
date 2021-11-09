84dc09d8f6d92b938443d87ccdf2ec44ca9bc8b3
PandaSekh <alessiofranceschi2@gmail.com>
2021-11-09 11:48:23 +0100

    update deps

--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,6 @@
+/**
+ * @jest-environment jsdom
+ */
 import loader from "../dist/index";
 import "@babel/polyfill";
 
@@ -6,13 +9,13 @@ describe("dynamic script loader", () => {
 		expect.assertions(1);
 		loader("./logger.js").then(script =>
 			expect(script.src).toBe("http://localhost/logger.js")
-		);
+		).catch(e => console.error(e));
 	}, 9999);
 
 	it("should return script even if already present", () => {
 		expect.assertions(1);
 		return loader("./logger.js").then(s =>
 			expect(s.src).toBe("http://localhost/logger.js")
-		);
+		).catch(e => console.error(e));
 	});
 });
